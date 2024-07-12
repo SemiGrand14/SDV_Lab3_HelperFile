b4ad7cf3501f44478d49204413a7ca4a4158b5ca
SemiGrand14 <132380490+SemiGrand14@users.noreply.github.com>
2024-07-12 06:05:04 -0400

    Added clearscreen()

--- Helper.h
+++ Helper.h
@@ -8,7 +8,8 @@ namespace Helper
 	int PrintMenu(const std::string* menuOptions[]); //Done
 	void ClearIgnoreBuffer(); //Done
 	void PrintInteger(int* valueToPrint); //Done
-	int GenerateRandomNumber(const int* minRange = 0, const int* maxRange=0); //TODO: Define method
+	int GenerateRandomNumber(const int* minRange = 0, const int* maxRange=0); //Done
+	void ClearScreen() { system("cls"); }; //clear the screen - done
 
 	//GetValidatedInt----------------------------------------------------------------------------------
 	int GetValidatedInt(const char* strMessage, int nMinimumRange, int nMaximumRange)
